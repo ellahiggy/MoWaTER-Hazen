ba434b8ce1abd5ac83eb284bfcd052c663c19b47
ellahiggy <84244453+ellahiggy@users.noreply.github.com>
2022-06-14 11:04:24 -0500

    Update Code_Collab_Hazen_&_Sawyer.R
    Initial time series plots -- spec flux, NDP, NPC

--- Code_Collab_Hazen_&_Sawyer.R
+++ Code_Collab_Hazen_&_Sawyer.R
@@ -74,3 +74,357 @@ Train_5 <-
 
 
 
+### Plots of Specific Flux, Normalized Differential Pressure, Permeate Conductivity for Stakeholders
+
+# This code creates 15 plots: One plot per train (five trains = five plots) for specific flux over time, 
+# normalized differential pressure over time, and permeate conductivity over time. 
+
+
+
+# swd 
+load("~/Desktop/DATA SCIENCE FELLOWS/Hazen and Sawyer/dataset/HSMaster - Hazen and Sawyer (1).rda")
+
+
+
+
+## Specific Flux ---------------------------------------------------------------
+
+# Train 1 ------------------------------------------
+# Comments for Train 1 Specific Flux are very similar for how the rest of the plots will run
+
+# Provides space for 2 rows, 3 columns for 5 plots
+par(mfrow = c(2, 3)) 
+
+# Creates a dataframe for Train 1
+Train_1 <- HSMaster[, c(1, 37:50, 121:137)]
+
+# Makes NA values into T/F, T/F into integers (0s and 1s), and takes mean of them to put them on plot
+na_values_1_spfl <- as.integer(is.na(Train_1$Sp_Fl_TR1))*mean(Train_1$Sp_Fl_TR1,na.rm=TRUE) 
+
+# Finds time of NA values
+na_time_1_spfl <- Train_1$Date_Time[is.na(Train_1$Sp_Fl_TR1)]
+
+# Plots specific flux over time, subtracting off the mean of the data 
+plot(Train_1$Date_Time, Train_1$Sp_Fl_TR1- median(Train_1$Sp_Fl_TR1, na.rm=TRUE), 
+     main = "Train 1 Specific Flux Over Time, with NA values", 
+     xlab = "Date", 
+     ylab = "Specific Flux", 
+     ylim = c(-0.02, 0.03), 
+     col = rgb(0, 0, 0, 0.5), 
+     pch = 19)
+
+# Adds lines for NA values
+abline(v = na_time_1_spfl, col = adjustcolor("magenta", alpha = 0.1))
+
+# repeat same code for rest of variables and trains
+
+
+
+# Train 2 ------------------------------------------
+
+Train_2 <- HSMaster[, c(1, 51:64, 138:154)]
+
+na_values_2_spfl <- as.integer(is.na(Train_2$Sp_Fl_TR2))*mean(Train_2$Sp_Fl_TR2,na.rm=TRUE) 
+
+na_time_2_spfl <- Train_2$Date_Time[is.na(Train_2$Sp_Fl_TR2)]
+
+plot(Train_2$Date_Time, Train_2$Sp_Fl_TR2 - median(Train_2$Sp_Fl_TR2, na.rm=TRUE), 
+     main = "Train 2 Specific Flux Over Time, with NA values", 
+     xlab = "Date", 
+     ylab = "Specific Flux", 
+     ylim = c(-0.01, 0.01),
+     col = rgb(0, 0, 0, 0.5), 
+     pch = 19, 
+)
+
+abline(v = na_time_2_spfl, col = adjustcolor("magenta", alpha = 0.2))
+
+
+
+# Train 3 ------------------------------------------
+
+Train_3 <- HSMaster[, c(1, 65:78, 155:171)]
+
+na_values_3_spfl <- as.integer(is.na(Train_3$Sp_Fl_TR3))*mean(Train_3$Sp_Fl_TR3,na.rm=TRUE) 
+
+na_time_3_spfl <- Train_3$Date_Time[is.na(Train_3$Sp_Fl_TR3)]
+
+plot(Train_3$Date_Time, Train_3$Sp_Fl_TR3 - median(Train_3$Sp_Fl_TR3, na.rm=TRUE), 
+     main = "Train 3 Specific Flux Over Time, with NA values", 
+     xlab = "Date", 
+     ylab = "Specific Flux", 
+     ylim = c(-0.01, 0.006),
+     col = rgb(0, 0, 0, 0.5), 
+     pch = 19)
+
+abline(v = na_time_3_spfl, col = adjustcolor("magenta", alpha = 0.1))
+
+
+
+# Train 4 ------------------------------------------
+
+Train_4 <- HSMaster[, c(1, 79:98, 117, 172:188)]
+
+na_values_4_spfl <- as.integer(is.na(Train_4$Sp_Fl_TR4))*mean(Train_4$Sp_Fl_TR4,na.rm=TRUE) 
+
+na_time_4_spfl <- Train_4$Date_Time[is.na(Train_4$Sp_Fl_TR4)]
+
+plot(Train_4$Date_Time, Train_4$Sp_Fl_TR4 - median(Train_4$Sp_Fl_TR4, na.rm=TRUE), 
+     main = "Train 4 Specific Flux Over Time, with NA values", 
+     xlab = "Date", 
+     ylab = "Specific Flux", 
+     ylim = c(-0.03, 0.03),
+     col = rgb(0, 0, 0, 0.5), 
+     pch = 19)
+
+abline(v = na_time_4_spfl, col = adjustcolor("magenta", alpha = 0.1))
+
+
+
+# Train 5 ------------------------------------------
+
+Train_5 <- HSMaster[, c(1, 99:116, 118, 189:205)]
+
+na_values_5_spfl <- as.integer(is.na(Train_5$Sp_Fl_TR5))*mean(Train_5$Sp_Fl_TR5,na.rm=TRUE) 
+
+na_time_5_spfl <- Train_5$Date_Time[is.na(Train_5$Sp_Fl_TR5)]
+
+plot(Train_5$Date_Time, Train_5$Sp_Fl_TR5 - median(Train_5$Sp_Fl_TR5, na.rm=TRUE), 
+     main = "Train 5 Specific Flux Over Time, with NA values", 
+     xlab = "Date", 
+     ylab = "Specific Flux", 
+     ylim = c(-0.016, 0.015),
+     col = rgb(0, 0, 0, 0.5), 
+     pch = 19)
+
+abline(v = na_time_5_spfl, col = adjustcolor("magenta", alpha = 0.1))
+
+#-------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+## Normalized Differential Pressure --------------------------------------------
+
+# Train 1 ------------------------------------------
+
+par(mfrow = c(2, 3)) 
+
+Train_1 <- HSMaster[, c(1, 37:50, 121:137)]
+
+na_values_1_ndp <- as.integer(is.na(Train_1$DP_n_TR1))*mean(Train_1$DP_n_TR1,na.rm=TRUE) 
+
+na_time_1_ndp <- Train_1$Date_Time[is.na(Train_1$DP_n_TR1)]
+
+plot(Train_1$Date_Time, Train_1$DP_n_TR1 - median(Train_1$DP_n_TR1, na.rm=TRUE), 
+     main = "Train 1 Normalized Differential Pressure Over Time, with NA values", 
+     xlab = "Date", 
+     ylab = "Normalized Differential Pressure", 
+     ylim = c(-3, 5),
+     col = rgb(0, 0, 0, 0.5), 
+     pch = 19)
+
+abline(v = na_time_1_ndp, col = adjustcolor("blue", alpha = 0.1))
+
+
+
+# Train 2 ------------------------------------------
+
+Train_2 <- HSMaster[, c(1, 51:64, 138:154)]
+
+na_values_2_ndp <- as.integer(is.na(Train_2$DP_n_TR2))*mean(Train_2$DP_n_TR2,na.rm=TRUE) 
+
+na_time_2_ndp <- Train_2$Date_Time[is.na(Train_2$DP_n_TR2)]
+
+plot(Train_2$Date_Time, Train_2$DP_n_TR2 - median(Train_2$DP_n_TR2, na.rm=TRUE), 
+     main = "Train 2 Normalized Differential Pressure Over Time, with NA values", 
+     xlab = "Date", 
+     ylab = "Normalized Differential Pressure", 
+     ylim = c(-3, 2),
+     col = rgb(0, 0, 0, 0.5), 
+     pch = 19)
+
+abline(v = na_time_2_ndp, col = adjustcolor("blue", alpha = 0.2))
+
+
+
+# Train 3 ------------------------------------------
+
+Train_3 <- HSMaster[, c(1, 65:78, 155:171)]
+
+na_values_3_ndp <- as.integer(is.na(Train_3$DP_n_TR3))*mean(Train_3$DP_n_TR3,na.rm=TRUE) 
+
+na_time_3_ndp <- Train_3$Date_Time[is.na(Train_3$DP_n_TR3)]
+
+plot(Train_3$Date_Time, Train_3$DP_n_TR3 - median(Train_3$DP_n_TR3, na.rm=TRUE), 
+     main = "Train 3 Normalized Differential Pressure Over Time, with NA values", 
+     xlab = "Date", 
+     ylab = "Normalized Differential Pressure", 
+     ylim = c(-2.7, 1.5),
+     col = rgb(0, 0, 0, 0.5), 
+     pch = 19)
+
+abline(v = na_time_3_ndp, col = adjustcolor("blue", alpha = 0.2))
+
+
+
+# Train 4 ------------------------------------------
+
+Train_4 <- HSMaster[, c(1, 79:98, 117, 172:188)]
+
+na_values_4_ndp <- as.integer(is.na(Train_4$DP_n_TR4))*mean(Train_4$DP_n_TR4,na.rm=TRUE) 
+
+na_time_4_ndp <- Train_4$Date_Time[is.na(Train_4$DP_n_TR4)]
+
+plot(Train_4$Date_Time, Train_4$DP_n_TR4 - median(Train_4$DP_n_TR4, na.rm=TRUE), 
+     main = "Train 4 Normalized Differential Pressure Over Time, with NA values", 
+     xlab = "Date", 
+     ylab = "Normalized Differential Pressure", 
+     ylim = c(-6, 8),
+     col = rgb(0, 0, 0, 0.5), 
+     pch = 19)
+
+abline(v = na_time_4_ndp, col = adjustcolor("blue", alpha = 0.2))
+
+
+
+# Train 5 ------------------------------------------
+
+Train_5 <- HSMaster[, c(1, 99:116, 118, 189:205)]
+
+na_values_5_ndp <- as.integer(is.na(Train_5$DP_n_TR5))*mean(Train_5$DP_n_TR5,na.rm=TRUE) 
+
+na_time_5_ndp <- Train_5$Date_Time[is.na(Train_5$DP_n_TR5)]
+
+plot(Train_5$Date_Time, Train_5$DP_n_TR5 - median(Train_5$DP_n_TR5, na.rm=TRUE), 
+     main = "Train 5 Normalized Differential Pressure Over Time, with NA values", 
+     xlab = "Date", 
+     ylab = "Normalized Differential Pressure", 
+     ylim = c(-4.5, 3),
+     col = rgb(0, 0, 0, 0.5), 
+     pch = 19)
+
+abline(v = na_time_5_ndp, col = adjustcolor("blue", alpha = 0.2))
+
+#-------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+## Normalized Permeate Conductivity --------------------------------------------
+
+# Train 1 ------------------------------------------
+
+par(mfrow = c(2, 3)) 
+
+Train_1 <- HSMaster[, c(1, 37:50, 121:137)]
+
+na_values_1_npc <- as.integer(is.na(Train_1$NCp_TR1))*mean(Train_1$NCp_TR1,na.rm=TRUE) 
+
+na_time_1_npc <- Train_1$Date_Time[is.na(Train_1$NCp_TR1)]
+
+plot(Train_1$Date_Time, Train_1$NCp_TR1 - median(Train_1$NCp_TR1, na.rm=TRUE), 
+     main = "Train 1 Normalized Permeate Conductivity Over Time, with NA values", 
+     xlab = "Date", 
+     ylab = "Normalized Permeate Conductivity", 
+     ylim = c(-10, 50),
+     col = rgb(0, 0, 0, 0.5), 
+     pch = 19)
+
+abline(v = na_time_1_npc, col = adjustcolor("orange", alpha = 0.1))
+
+
+
+# Train 2 ------------------------------------------
+
+Train_2 <- HSMaster[, c(1, 51:64, 138:154)]
+
+na_values_2_npc <- as.integer(is.na(Train_2$NCp_TR2))*mean(Train_2$NCp_TR2,na.rm=TRUE) 
+
+na_time_2_npc <- Train_2$Date_Time[is.na(Train_2$NCp_TR2)]
+
+plot(Train_2$Date_Time, Train_2$NCp_TR2 - median(Train_2$NCp_TR2, na.rm=TRUE), 
+     main = "Train 2 Normalized Permeate Conductivity Over Time
+     (NA values in orange)", 
+     xlab = "Date", 
+     ylab = "Normalized Permeate Conductivity", 
+     ylim = c(-6, 5),
+     col = rgb(0, 0, 0, 0.5), 
+     pch = 19, 
+)
+
+abline(v = na_time_2_npc, col = adjustcolor("orange", alpha = 0.1))
+
+
+
+# Train 3 ------------------------------------------
+
+Train_3 <- HSMaster[, c(1, 65:78, 155:171)]
+
+na_values_3_npc <- as.integer(is.na(Train_3$NCp_TR3))*mean(Train_3$NCp_TR3,na.rm=TRUE) 
+
+na_time_3_npc <- Train_2$Date_Time[is.na(Train_3$NCp_TR3)]
+
+plot(Train_3$Date_Time, Train_3$NCp_TR3 - median(Train_3$NCp_TR3, na.rm=TRUE), 
+     main = "Train 3 Normalized Permeate Conductivity Over Time, with NA values", 
+     xlab = "Date", 
+     ylab = "Normalized Permeate Conductivity", 
+     ylim = c(-2, 2),
+     col = rgb(0, 0, 0, 0.5), 
+     pch = 19)
+
+abline(v = na_time_3_npc, col = adjustcolor("orange", alpha = 0.2))
+
+
+
+# Train 4 ------------------------------------------
+
+Train_4 <- HSMaster[, c(1, 79:98, 117, 172:188)]
+
+na_values_4_npc <- as.integer(is.na(Train_4$NCp_TR4))*mean(Train_4$NCp_TR4,na.rm=TRUE) 
+
+na_time_4_npc <- Train_4$Date_Time[is.na(Train_4$NCp_TR4)]
+
+plot(Train_4$Date_Time, Train_4$NCp_TR4 - median(Train_4$NCp_TR4, na.rm=TRUE), 
+     main = "Train 4 Normalized Permeate Conductivity Over Time, with NA values", 
+     xlab = "Date", 
+     ylab = "Normalized Permeate Conductivity", 
+     ylim = c(35, 50),
+     col = rgb(0, 0, 0, 0.5), 
+     pch = 19)
+
+abline(v = na_time_4_npc, col = adjustcolor("orange", alpha = 0.2))
+
+
+
+# Train 5 ------------------------------------------
+
+Train_5 <- HSMaster[, c(1, 99:116, 118, 189:205)]
+
+na_values_5_npc <- as.integer(is.na(Train_5$NCp_TR5))*mean(Train_5$NCp_TR5,na.rm=TRUE) 
+
+na_time_5_npc <- Train_4$Date_Time[is.na(Train_5$NCp_TR5)]
+
+plot(Train_5$Date_Time, Train_5$NCp_TR5 - median(Train_5$NCp_TR5, na.rm=TRUE), 
+     main = "Train 5 Normalized Permeate Conductivity Over Time, with NA values", 
+     xlab = "Date", 
+     ylab = "Normalized Permeate Conductivity", 
+     ylim = c(26, 36),
+     col = rgb(0, 0, 0, 0.5), 
+     pch = 19)
+
+abline(v = na_time_5_npc, col = adjustcolor("orange", alpha = 0.2))
