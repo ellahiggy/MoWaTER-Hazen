15b81029881e882e27dca31428c91f6f1a70cee3
HenryBurch1 <82896092+HenryBurch1@users.noreply.github.com>
2022-06-14 09:12:24 -0700

    Update Code_Collab_Hazen_&_Sawyer.R

--- Code_Collab_Hazen_&_Sawyer.R
+++ Code_Collab_Hazen_&_Sawyer.R
@@ -539,4 +539,96 @@ legend("topleft",                                 # Add legend to plot
        cex = 0.48,
        horiz = TRUE)
 
+# --------------------------------------------------------------------------------------------------------------
+## Filter_Standardize_Train1
+
+## New Standardized Data set for Train one ---
+
+# List of variables to use
+vars <- c("Date_Time","Nt_DP_TR1","Sp_Fl_TR1","NCp_TR1","Per_R_a_TR1","P_f_TR1")
+
+# Initiate data frame from the Train_1 data frame
+Train_1_standardized <- Train_1[vars]
+
+# Replace each value in the Train_1 data frame with standardized values 
+for (column_num in c(2:6)) {
+  Train_1_standardized[,column_num] <- scale(Train_1_standardized[,column_num])
+}
+# ------------------------------------------
+
+### December through March
+
+# Create data frame which selects the time period of December through March and 
+# omit all NA values. We will use this data to plot the time around the 
+# potential clean in place/membrane change. Use the data which has been standardized
+
+df <- na.omit(Train_1_standardized[Train_1_standardized$Date_Time >= "2020-12-01 00:00:00" & 
+                                 Train_1_standardized$Date_Time <= "2021-03-30 02:00:00",])
+
+
+
+plot(df$Date_Time,                                # Net Driving Pressure
+     df$Nt_DP_TR1_scaled,
+     col = 1,
+     ylim = c(-5,5),
+     xlab = "Date",
+     ylab = "Variables",
+     main = "Variables over Time")
+points(df$Date_Time,                              # Specific Flux
+       df$Sp_Fl_TR1_scaled,
+       col = "orange")
+points(df$Date_Time,                              # Permeate Conductivity
+       df$NCp_TR1_scaled,
+       col = 3)
+points(df$Date_Time,                              # % Conductivity
+       df$Per_R_a_TR1_scaled,
+       col = 4)
+points(df$Date_Time,                              # Feedwater Pressure
+       df$P_f_TR1_scaled,
+       col = 6)
+
+legend("topleft",                                 # Add legend to plot
+       c("Net driving pressure", "Specific flux", "Permeate conductivity","% Recovery","Feedwater Pressure"),
+       lty = 1,
+       col = c("black","orange", 3,4,6),
+       cex = 0.48,
+       horiz = TRUE)
+
+
+# Create data frame which selects the time period of April through May and 
+# omit all NA values. We will use this data to plot the time around the 
+# potential clean in place/membrane change. Use the data which has been standardized
+
+df <- na.omit(Train_1_standardized[Train_1_standardized$Date_Time >= "2021-04-01 00:00:00" & 
+                                 Train_1_standardized$Date_Time <= "2021-05-30 00:00:00",])
+
+
+
+plot(df$Date_Time,                                # Bet Driving Pressure
+     df$Nt_DP_TR1_scaled,
+     col = 1,
+     ylim = c(-5,5),
+     xlab = "Date",
+     ylab = "Variables",
+     main = "Variables over Time")
+points(df$Date_Time,                              # Specific Flux
+       df$Sp_Fl_TR1_scaled,
+       col = "orange")
+points(df$Date_Time,                              # Permeate Conductivity
+       df$NCp_TR1_scaled,
+       col = 3)
+points(df$Date_Time,                              # % Recovery
+       df$Per_R_a_TR1_scaled,
+       col = 4)
+points(df$Date_Time,                              # Feed water Pressure 
+       df$P_f_TR1_scaled,
+       col = 6)
+
+legend("topleft",                                 # Add legend to plot
+       c("Net driving pressure", "Specific flux", "Permeate conductivity","% Recovery","Feedwater Pressure"),
+       lty = 1,
+       col = c("black","orange", 3,4,6),
+       cex = 0.48,
+       horiz = TRUE)
+
 
