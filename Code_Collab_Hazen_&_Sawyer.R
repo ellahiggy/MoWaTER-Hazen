418b4e056105325c3941f29a607c3439fd324c51
cypill36 <cyril_pillai1@baylor.edu>
2022-06-14 10:51:56 -0500

    code collab for code swap

---
+++ Code_Collab_Hazen_&_Sawyer.R
@@ -0,0 +1,25 @@
+# -------------------------------------------------------------------------
+# MOWATER 
+# Hazen and Sawyer Team
+# Code Collab
+# Henry Burch, PJ Williams, Ella Higginbotham, Lauren Walker, Cyril Pillai
+# 
+# Narrowed Variables List, x = train number
+# Time Log - Date_Time
+# Percent Salt Passage - SP_a_TRx
+# Specific Flux - Sp_Fl_TRx
+# Net Driving pressure - Nt_DP_TRx 
+# Net Driving Pressure at Actual Conditions (Train) - NetDp_a_TRx
+# Normalized permeate conductivity - NCp_TRx
+# Permeate Conductivity - C_p_TRx
+# T Concentrate Conductivity - C_c_TRx
+# % Recovery - Per_R_a_TR1
+# Recovery at Actual Conditions - R_a_TRx
+# Feedwater Pressure - P_f_TRx
+# Online Status - ON_TRx
+# Normalized DIfferential Pressure - DP_n_TRx
+# Feedwater Temperature - T_f_CO
+# Temperature Correction Factor at Actual Conditions - TCF_a
+#  -------------------------------------------------------------------------
+
+
