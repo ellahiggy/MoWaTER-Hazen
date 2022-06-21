a6a9fc6c242fe74cf42ee865b198bb8033cad238
cypill36 <cyril_pillai1@baylor.edu>
2022-06-21 10:47:14 -0500

    column name standardization

--- raw_to_clean_data.R
+++ raw_to_clean_data.R
@@ -32,34 +32,35 @@ load("HSMaster - Hazen and Sawyer.rda")
 #Data general to all trains
 General <- HSMaster[, c("Date_Time", "T_f_CO", "TCF_a")]
 
+vars <- c("Date_Time","Sp_Fl","Nt_DP","NCp","Per_R_a","P_f","SP_n","ON_OFF","Index")
+
 
 #Data for each variable by Train, as detailed above
-Train_1 <-
-             HSMaster[,c("Date_Time", "SP_a_TR1", "Sp_Fl_TR1", "Nt_DP_TR1", 
-                         "NetDp_a_TR1", "NCp_TR1", "C_p_TR1", "C_c_TR1", 
-                         "Per_R_a_TR1", "R_a_TR1", "P_f_TR1", "ON_TR1", 
-                         "DP_n_TR1")]
+Train_1 <- HSMaster[,c("Date_Time", "Sp_Fl_TR1", "Nt_DP_TR1", 
+                       "NCp_TR1", "Per_R_a_TR1", "P_f_TR1","SP_n_TR1","ON_TR1")]
+Train_1$index <- 1:nrow(Train_1)
+colnames(Train_1) <- vars
 
-Train_2 <-
-            HSMaster[,c("Date_Time", "SP_a_TR2", "Sp_Fl_TR2", "Nt_DP_TR2", 
-                        "NetDp_a_TR2", "NCp_TR2", "C_p_TR2", "C_c_TR2", 
-                        "Per_R_a_TR2", "R_a_TR2", "P_f_TR2", "ON_TR2", 
-                        "DP_n_TR2")]
-Train_3 <-
-            HSMaster[,c("Date_Time", "SP_a_TR3", "Sp_Fl_TR3", "Nt_DP_TR3", 
-                        "NetDp_a_TR3", "NCp_TR3", "C_p_TR3", "C_c_TR3", 
-                        "Per_R_a_TR3", "R_a_TR3", "P_f_TR3", "ON_TR3", 
-                        "DP_n_TR3")]
-Train_4 <-
-            HSMaster[,c("Date_Time", "SP_a_TR4", "Sp_Fl_TR4", "Nt_DP_TR4", 
-                        "NetDp_a_TR4", "NCp_TR4", "C_p_TR4", "C_c_TR4", 
-                        "Per_R_a_TR4", "R_a_TR4", "P_f_TR4", "ON_TR4", 
-                        "DP_n_TR4")]
-Train_5 <-
-            HSMaster[,c("Date_Time", "SP_a_TR5", "Sp_Fl_TR5", "Nt_DP_TR5", 
-                        "NetDp_a_TR5", "NCp_TR5", "C_p_TR5", "C_c_TR5", 
-                        "Per_R_a_TR5", "R_a_TR5", "P_f_TR5", "ON_TR5", 
-                        "DP_n_TR5")]
+Train_2 <- HSMaster[,c("Date_Time", "Sp_Fl_TR2", "Nt_DP_TR2", 
+                       "NCp_TR2", "Per_R_a_TR2", "P_f_TR2","SP_n_TR2","ON_TR2")]
+Train_2$index <- 1:nrow(Train_2)
+colnames(Train_2) <- vars
+
+Train_3 <- HSMaster[,c("Date_Time", "Sp_Fl_TR3", "Nt_DP_TR3", 
+                       "NCp_TR3", "Per_R_a_TR3", "P_f_TR3","SP_n_TR3","ON_TR3")]
+Train_3$index <- 1:nrow(Train_3)
+colnames(Train_3) <- vars
+
+Train_4 <- HSMaster[,c("Date_Time", "Sp_Fl_TR4", "Nt_DP_TR4", 
+                       "NCp_TR4", "Per_R_a_TR4", "P_f_TR4","SP_n_TR4",'ON_TR4')]
+Train_4$index <- 1:nrow(Train_4)
+colnames(Train_4) <- vars
+
+Train_5 <- HSMaster[,c("Date_Time", "Sp_Fl_TR5", "Nt_DP_TR5", 
+                       "NCp_TR5", "Per_R_a_TR5", "P_f_TR5","SP_n_TR5","ON_TR5")]
+Train_5$index <- 1:nrow(Train_5)
+colnames(Train_5) <- vars
+
 
 # sum(is.null(Train_1)) # 0
 # sum(is.null(Train_2)) # 0
@@ -67,5 +68,11 @@ Train_5 <-
 # sum(is.null(Train_4)) # 0
 # sum(is.null(Train_5)) # 0
 # No null values in the dataset, all done!
+
+#clean df with all variables we created 
+#list of dataframes
+#save as .rda object
+
+
 
 
