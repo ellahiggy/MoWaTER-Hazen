954087ac8c76d0811cc6b7925742a18acd078aa5
cypill36 <cyril_pillai1@baylor.edu>
2022-06-27 14:44:02 -0500

    raw_to_clean udpates

--- raw_to_clean_data.R
+++ raw_to_clean_data.R
@@ -6,20 +6,13 @@
 # 
 # Narrowed Variables List, x = train number
 # Time Log - Date_Time
-# Percent Salt Passage - SP_a_TRx
 # Specific Flux - Sp_Fl_TRx
 # Net Driving pressure - Nt_DP_TRx 
-# Net Driving Pressure at Actual Conditions (Train) - NetDp_a_TRx
 # Normalized permeate conductivity - NCp_TRx
-# Permeate Conductivity - C_p_TRx
-# T Concentrate Conductivity - C_c_TRx
 # % Recovery - Per_R_a_TR1
-# Recovery at Actual Conditions - R_a_TRx
 # Feedwater Pressure - P_f_TRx
+# Percent Salt Passage - SP_a_TRx
 # Online Status - ON_TRx
-# Normalized DIfferential Pressure - DP_n_TRx
-# Feedwater Temperature - T_f_CO
-# Temperature Correction Factor at Actual Conditions - TCF_a
 #  -------------------------------------------------------------------------
 
 
@@ -28,9 +21,6 @@
 # HSMaster - Hazen and Sawyer.rda"
 setwd("/Users/cyrilpillai/Desktop/Data_Processing")
 load("HSMaster - Hazen and Sawyer.rda")
-
-#Data general to all trains
-General <- HSMaster[, c("Date_Time", "T_f_CO", "TCF_a")]
 
 vars <- c("Date_Time","Sp_Fl","Nt_DP","NCp","Per_R_a","P_f","SP_n","ON_OFF","Index")
 
@@ -70,40 +60,46 @@ colnames(Train_5) <- vars
 # No null values in the dataset, all done!
 
 
-#Function to get time periods when each train is on and off
-
-
-train_ONOFF <- function(Train_X)
+#Function to get time periods when a train is on and off
+train_ONOFF <- function(number)
 {
-  if(!is.data.frame(Train_X)) stop("Train_X must be a df")
   
-  data_ONOFF <- Train_X
+  Trains <- list(Train_1, Train_2, Train_3, Train_4, Train_5)
+  
+  data_ONOFF <- Trains[[number]]
   
   data_ONOFF$ON_Count <- 0
   #data_ONOFF[is.na(data_ONOFF)] = 0
   #data_ONOFF[is.null(data_ONOFF)] = 0
-  off <- 0
-  on <- 0
+
   
   len <- nrow(data_ONOFF)
   
+  off <- 0
+  on <- 0
+  
+  
   for (num in 1:len) {
+    
     if (data_ONOFF$ON_OFF[num] == 0|is.na(data_ONOFF$ON_OFF[num])){
       data_ONOFF$ON_Count[num] <- 0
     }else {
       if (num == 1){
-        on <<- on + 1
+        on <- on + 1
       }else if (data_ONOFF$ON_OFF[num-1] == 0|is.na(data_ONOFF$ON_OFF[num-1])){
-        on <<- on + 1
+        on <- on + 1
       }
       data_ONOFF$ON_Count[num] <- on
     }
   }
   
   Data_ON_Number <-  unique(data_ONOFF$ON_Count)
+  
   Data_ON_Number <- Data_ON_Number[Data_ON_Number != 0]; # without elements that are "b"
   
-  Data_ON_Change <- as.data.frame(matrix(nrow = max(Data_ON_Number), ncol = 3))
+  max_ON <- max(Data_ON_Number)
+  
+  Data_ON_Change <- as.data.frame(matrix(nrow = max_ON, ncol = 3))
   colnames(Data_ON_Change) <- c("ON_Count","Start_Index","End_Index")
 
   for (num in Data_ON_Number){
@@ -123,35 +119,16 @@ train_ONOFF <- function(Train_X)
 }
 
 
-Train5_ON_Change <- train_ONOFF(Train_5)
+#data frames with intervals in which each train is on
+Train1_ON_Change <- train_ONOFF(1)
+Train2_ON_Change <- train_ONOFF(2)
+Train3_ON_Change <- train_ONOFF(3)
+Train4_ON_Change <- train_ONOFF(4)
+Train5_ON_Change <- train_ONOFF(5)
 
 
-
-
-
-
-
-
-
-
-
-
-# Variables for EDA Plots
-
-# Variables for five train plots of specific flux plots with values for train = OFF
-na_values_1_spfl <- as.integer(is.na(Train_1$Sp_Fl_TR1))*mean(Train_1$Sp_Fl_TR1,na.rm=TRUE) 
-na_time_1_spfl <- Train_1$Date_Time[is.na(Train_1$Sp_Fl_TR1)]
-
-na_values_2_spfl <- as.integer(is.na(Train_2$Sp_Fl_TR2))*mean(Train_2$Sp_Fl_TR2,na.rm=TRUE) 
-na_time_2_spfl <- Train_2$Date_Time[is.na(Train_2$Sp_Fl_TR2)]
-
-na_values_3_spfl <- as.integer(is.na(Train_3$Sp_Fl_TR3))*mean(Train_3$Sp_Fl_TR3,na.rm=TRUE) 
-na_time_3_spfl <- Train_3$Date_Time[is.na(Train_3$Sp_Fl_TR3)]
-
-na_values_4_spfl <- as.integer(is.na(Train_4$Sp_Fl_TR4))*mean(Train_4$Sp_Fl_TR4,na.rm=TRUE) 
-na_time_4_spfl <- Train_4$Date_Time[is.na(Train_4$Sp_Fl_TR4)]
-
-na_values_5_spfl <- as.integer(is.na(Train_5$Sp_Fl_TR5))*mean(Train_5$Sp_Fl_TR5,na.rm=TRUE) 
-na_time_5_spfl <- Train_5$Date_Time[is.na(Train_5$Sp_Fl_TR5)]
-
+save(Train_1, Train_2, Train_3, Train_4, Train_5, 
+     Train1_ON_Change, Train2_ON_Change, Train3_ON_Change,
+     Train4_ON_Change, Train5_ON_Change,
+     file = "Hazen_Sawyer_Data_Clean.rda")
 
